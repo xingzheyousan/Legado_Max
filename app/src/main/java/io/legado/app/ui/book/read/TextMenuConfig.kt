06ffed512c997d8c09bce27ae71d3df3bfa49aa1
youfengknight <youfeng.knight@qq.com>
2026-06-01 17:54:25 +0800

    添加网页搜索文本菜单项

--- app/src/main/java/io/legado/app/ui/book/read/TextMenuConfig.kt
+++ app/src/main/java/io/legado/app/ui/book/read/TextMenuConfig.kt
@@ -46,6 +46,7 @@ object TextMenuConfig {
         MenuItemInfo(R.id.menu_aloud, R.string.read_aloud),
         MenuItemInfo(R.id.menu_dict, R.string.dict),
         MenuItemInfo(R.id.menu_search_content, R.string.search_content),
+        MenuItemInfo(R.id.menu_web_search, R.string.web_search),
         MenuItemInfo(R.id.menu_browser, R.string.browser),
         MenuItemInfo(R.id.menu_share_str, R.string.share)
     )
